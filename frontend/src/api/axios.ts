c9c3bfade0d79eb0c5527b7b48d3ad787515a35e
Danish Tyagi <tyagidanish8549@gmail.com>
2026-05-06 07:44:39 -0700

    Update axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://teamsync-production-491a.up.railway.app/api',
+  baseURL: 'https://teamsync-production-491a.up.railway.app/',
   headers: {
     'Content-Type': 'application/json',
   },
